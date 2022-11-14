5be89731b8a636e328c1738f32002d0482c96600
gauravgangwarweb <gauravgangwar501@gmail.com>
2022-11-14 19:22:12 +0530

    Add popup menu to nav bar

---
+++ main.js
@@ -0,0 +1,8 @@
+function toggle() {
+    console.log("Here am i")
+    document.getElementById("menupopup").style.display ="flex";
+}
+
+function close1() {
+    document.getElementById("menupopup").style.display ="none";
+}
